3054971e219f18f819fd2df20d0a856be2aaf07d
Forbes Lindesay <forbes@lindesay.co.uk>
2021-10-18 14:46:27 +0100

    feat: initial code (#1)

--- README.md
+++ README.md
@@ -1,43 +1,38 @@
-# npm-package-template
+# jwt-cache
 
-A template for npm packages built in TypeScript
+Cache an async function for generating a JSON Web Token
 
-[![Rolling Versions](https://img.shields.io/badge/Rolling%20Versions-Enabled-brightgreen)](https://rollingversions.com/ForbesLindesay/npm-package-template)
-
-
-## Setting Up the New Repo
-
-1. Hit "Use This Template" to create the repository
-1. Enable [CircleCI](https://circleci.com/add-projects/gh/ForbesLindesay)
-1. Enable [Change Log Version](https://changelogversion.com) using [My Change Log Version Installation](https://github.com/settings/installations/7328191)
-1. In Settings
-   1. Disable "Wikis"
-   1. Disable "Projects"
-   1. Disable "Allow merge commits"
-   1. Disable "Allow rebase merging"
-   1. Enable "Automatically delete head branches"
-1. Create a new branch
-1. Commit initial code to the branch (be sure to replace all refernces to npm-package-template, and remove these instructions from the README)
-1. Push the new branch and create a PR
-1. In Settings -> Branch Protection, create a new rule
-   1. Use "master" as the branch name pattern
-   1. Enable "Require status checks to pass before merging"
-   1. Select the unit tests and changelog as required
-   1. Enable "Include administrators"
-   1. Enable "Restrict who can push to matching branches"
-1. Merge the PR
+[![Rolling Versions](https://img.shields.io/badge/Rolling%20Versions-Enabled-brightgreen)](https://rollingversions.com/ForbesLindesay/jwt-cache)
 
 ## Installation
 
 ```
-yarn add @forbeslindesay/npm-package-template
+yarn add jwt-cache
 ```
 
 ## Usage
 
 ```ts
-import add from '@forbeslindesay/npm-package-template';
+import jwtCache from 'jwt-cache';
 
-const result = add(2, 3);
-// => 5
+const cache = jwtCache({
+  getToken: async () => {
+    // this could be slow code for generating a JSON Web Token
+    // with an expiry represented via the standard "exp" value
+    return sign({hello: 'world'}, `my_secret`, {expiresIn: 3});
+  },
+  // If the token will not be valid for this many ms, get
+  // a new token rather than returning the cached token
+  // Defaults to 1 second
+  minimumValidityMilliseconds: 100,
+  // Optionally eagerly fetch a token before it is needed.
+  // This will trigger fetching a new token this many ms
+  // before it would reach the minimum validity threshold
+  eagerRefreshMilliseconds: 100,
+});
+
+// When you need a token:
+cache.getToken().then((token) => {
+  // use the token here
+});
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,125 @@
-export default function add(a: number, b: number) {
-  return a + b;
+import decode from 'jwt-decode';
+
+export interface JwtCacheOptions {
+  getToken(): Promise<string>;
+  /**
+   * Defaults to 0 (i.e. disabled)
+   */
+  eagerRefreshMilliseconds?: number;
+  /**
+   * Defaults to 1 second
+   */
+  minimumValidityMilliseconds?: number;
+}
+interface State {
+  token: string;
+  expiry: number;
+}
+export default function jwtCache({
+  getToken,
+  eagerRefreshMilliseconds = 0,
+  minimumValidityMilliseconds = 1_000,
+}: JwtCacheOptions) {
+  let disposed = false;
+  let statePromise: undefined | Promise<State>;
+  let nextState: undefined | Promise<State>;
+  let timeout: any;
+  const getState = async (): Promise<State> => {
+    const token = await getToken();
+    const decoded = decode(token);
+    if (
+      typeof decoded === 'object' &&
+      decoded !== null &&
+      typeof (decoded as any).exp === 'number'
+    ) {
+      const expiry = (decoded as any).exp * 1000;
+      return {token, expiry};
+    } else {
+      throw new Error(
+        `You can only cache a JWT if it has an "exp" that is an integer number of seconds`,
+      );
+    }
+  };
+
+  const handleEagerRefresh = (
+    state: State,
+    currentStatePromise: Promise<State>,
+  ) => {
+    if (disposed) return;
+    if (eagerRefreshMilliseconds) {
+      timeout = setTimeout(() => {
+        if (statePromise === currentStatePromise) {
+          const thisNextState = (nextState = getState());
+
+          thisNextState.then(
+            (state) => {
+              if (
+                nextState === thisNextState &&
+                statePromise === currentStatePromise
+              ) {
+                statePromise = thisNextState;
+                handleEagerRefresh(state, thisNextState);
+              }
+            },
+            (_ex) => {
+              if (
+                nextState === thisNextState &&
+                statePromise === currentStatePromise
+              ) {
+                nextState = undefined;
+              }
+            },
+          );
+        }
+      }, state.expiry - Date.now() - minimumValidityMilliseconds - eagerRefreshMilliseconds);
+    }
+  };
+
+  const getUpdatedToken = async (
+    currentStatePromise: Promise<State> | undefined,
+  ) => {
+    if (statePromise && currentStatePromise !== statePromise) {
+      return (await statePromise).token;
+    }
+    statePromise = nextState ?? getState();
+    nextState = undefined;
+    try {
+      const state = await statePromise;
+      handleEagerRefresh(state, statePromise);
+      return state.token;
+    } catch (ex) {
+      statePromise = undefined;
+      throw ex;
+    }
+  };
+
+  if (eagerRefreshMilliseconds) {
+    getUpdatedToken(undefined).catch(() => {
+      // ignore error until a token is actually requested
+    });
+  }
+
+  return {
+    dispose: () => {
+      disposed = true;
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    },
+    getToken: async () => {
+      if (disposed) {
+        throw new Error(`Cannot get token after disposing jwt-cache`);
+      }
+      const stateP = statePromise;
+      if (!stateP) {
+        return await getUpdatedToken(stateP);
+      }
+
+      const state = await stateP;
+      if (state.expiry - Date.now() >= minimumValidityMilliseconds) {
+        return state.token;
+      }
+      return await getUpdatedToken(stateP);
+    },
+  };
 }
