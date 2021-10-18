3042e59cc799bee0a89942fbb2fd81f88ab5053b
Forbes Lindesay <forbes@lindesay.co.uk>
2021-10-18 14:37:23 +0100

    feat: initial code

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,87 @@
-import add from '../';
+import {sign, verify} from 'jsonwebtoken';
+import jwtCache from '../';
 
-test('add', () => {
-  expect(add(1, 2)).toBe(3);
+jest.setTimeout(30_000);
+
+test('jwtCache - eager', async () => {
+  let calls = 0;
+  const cache = jwtCache({
+    getToken: async () => {
+      calls++;
+      return sign({hello: 'world'}, `my_secret`, {expiresIn: 3});
+    },
+    minimumValidityMilliseconds: 100,
+    eagerRefreshMilliseconds: 500,
+  });
+
+  const tokens = await Promise.all([
+    cache.getToken(),
+    cache.getToken(),
+    cache.getToken(),
+  ]);
+
+  expect(calls).toBe(1);
+  for (const t of tokens) {
+    verify(t, 'my_secret');
+  }
+
+  await delay(3_100);
+  for (const t of tokens) {
+    expect(() => verify(t, 'my_secret')).toThrow();
+  }
+  expect(calls).toBe(2);
+
+  const tokens2 = await Promise.all([
+    cache.getToken(),
+    cache.getToken(),
+    cache.getToken(),
+  ]);
+  for (const t of tokens2) {
+    verify(t, 'my_secret');
+  }
+  expect(calls).toBe(2);
 });
+
+test('jwtCache - lazy', async () => {
+  let calls = 0;
+  const cache = jwtCache({
+    getToken: async () => {
+      calls++;
+      return sign({hello: 'world'}, `my_secret`, {expiresIn: 3});
+    },
+    minimumValidityMilliseconds: 100,
+  });
+  expect(calls).toBe(0);
+
+  const tokens = await Promise.all([
+    cache.getToken(),
+    cache.getToken(),
+    cache.getToken(),
+  ]);
+
+  expect(calls).toBe(1);
+  for (const t of tokens) {
+    verify(t, 'my_secret');
+  }
+
+  await delay(3_100);
+  for (const t of tokens) {
+    expect(() => verify(t, 'my_secret')).toThrow();
+  }
+  expect(calls).toBe(1);
+
+  const tokens2 = await Promise.all([
+    cache.getToken(),
+    cache.getToken(),
+    cache.getToken(),
+  ]);
+  expect(calls).toBe(2);
+
+  for (const t of tokens2) {
+    verify(t, 'my_secret');
+  }
+});
+
+async function delay(ms: number) {
+  await new Promise((r) => setTimeout(r, ms));
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,113 @@
-export default function add(a: number, b: number) {
-  return a + b;
+import decode from 'jwt-decode';
+
+export interface JwtCacheOptions {
+  getToken(): Promise<string>;
+  /**
+   * Defaults to 0 (i.e. disabled)
+   */
+  eagerRefreshMilliseconds?: number;
+  /**
+   * Defaults to 1 second
+   */
+  minimumValidityMilliseconds?: number;
+}
+interface State {
+  token: string;
+  expiry: number;
+}
+export default function jwtCache({
+  getToken,
+  eagerRefreshMilliseconds = 0,
+  minimumValidityMilliseconds = 1_000,
+}: JwtCacheOptions) {
+  let statePromise: undefined | Promise<State>;
+  let nextState: undefined | Promise<State>;
+  const getState = async (): Promise<State> => {
+    const token = await getToken();
+    const decoded = decode(token);
+    if (
+      typeof decoded === 'object' &&
+      decoded !== null &&
+      typeof (decoded as any).exp === 'number'
+    ) {
+      const expiry = (decoded as any).exp * 1000;
+      return {token, expiry};
+    } else {
+      throw new Error(
+        `You can only cache a JWT if it has an "exp" that is an integer number of seconds`,
+      );
+    }
+  };
+
+  const handleEagerRefresh = (
+    state: State,
+    currentStatePromise: Promise<State>,
+  ) => {
+    if (eagerRefreshMilliseconds) {
+      setTimeout(() => {
+        if (statePromise === currentStatePromise) {
+          const thisNextState = (nextState = getState());
+
+          thisNextState.then(
+            (state) => {
+              if (
+                nextState === thisNextState &&
+                statePromise === currentStatePromise
+              ) {
+                statePromise = thisNextState;
+                handleEagerRefresh(state, thisNextState);
+              }
+            },
+            (_ex) => {
+              if (
+                nextState === thisNextState &&
+                statePromise === currentStatePromise
+              ) {
+                nextState = undefined;
+              }
+            },
+          );
+        }
+      }, state.expiry - Date.now() - minimumValidityMilliseconds - eagerRefreshMilliseconds);
+    }
+  };
+
+  const getUpdatedToken = async (
+    currentStatePromise: Promise<State> | undefined,
+  ) => {
+    if (statePromise && currentStatePromise !== statePromise) {
+      return (await statePromise).token;
+    }
+    statePromise = nextState ?? getState();
+    nextState = undefined;
+    try {
+      const state = await statePromise;
+      handleEagerRefresh(state, statePromise);
+      return state.token;
+    } catch (ex) {
+      statePromise = undefined;
+      throw ex;
+    }
+  };
+
+  if (eagerRefreshMilliseconds) {
+    getUpdatedToken(undefined).catch(() => {
+      // ignore error until a token is actually requested
+    });
+  }
+
+  return {
+    getToken: async () => {
+      const stateP = statePromise;
+      if (!stateP) {
+        return await getUpdatedToken(stateP);
+      }
+
+      const state = await stateP;
+      if (state.expiry - Date.now() >= minimumValidityMilliseconds) {
+        return state.token;
+      }
+      return await getUpdatedToken(stateP);
+    },
+  };
 }
